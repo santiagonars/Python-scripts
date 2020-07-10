370f4502a509a0b924f0bedadfec24293c4706b2
Santiago Norena <san.norena@gmail.com>
2020-07-09 20:10:59 -0400

    update file name

--- copypaste-files.py
+++ copypaste-files.py
@@ -15,7 +15,7 @@ def main():
         full_filename = os.path.join(src, filename)
         #os.path.isfile() to see if they are regular files
         #statement does not copy files that are already in destination
-        if (os.path.isfile(full_filename)) and (filename != "copyfiles.py") and (filename not in dest_files):
+        if (os.path.isfile(full_filename)) and (filename != "copypaste-files.py") and (filename not in dest_files):
             #Copy a file with new name
             shutil.copy(full_filename, dest)
             print(filename)
